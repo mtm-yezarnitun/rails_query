d6640242000648605919a3af0443ceb5ad9c5924
P-000306\Ye Zar Ni Tun <yezarnitun@metateammyanmar.com>
2025-10-24 09:22:27 +0630

    :setup model/migration

---
+++ db/migrate/20251024024634_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :orders do |t|
+      t.time :date_submitted
+      t.integer :status
+      t.decimal :subtotal, precision: 10, scale: 2
+      t.decimal :shipping, precision: 10, scale: 2
+      t.decimal :tax, precision: 10, scale: 2
+      t.decimal :total, precision: 10, scale: 2
+      
+      t.references :customer, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
